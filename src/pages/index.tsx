2147273d0eca55cf72a8a9ab7467b7cdb82b4c41
jarilton <jariltonpereira02@hotmail.com>
2022-10-24 22:09:47 -0300

    Estilos globais com o stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { styled } from "../styles";
 
 const Button = styled("button", {
-  backgroundColor: "$rocket",
+  backgroundColor: "$green500",
   borderRadius: 5,
   border: 0,
   padding: "4px 8px",
