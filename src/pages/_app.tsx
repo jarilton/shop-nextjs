f2eb87da686f68810b83338af4bab56bc9c73735
jarilton <jariltonpereira02@hotmail.com>
2022-10-24 22:34:40 -0300

    Cabeçalho do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 
+import logoIMG from "../assets/Logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Container>
+        <Header>
+          <img src={logoIMG.src} alt="Logo do projeto" />
+        </Header>
+      </Container>
+      <Component {...pageProps} />
+    </div>
+  );
 }
