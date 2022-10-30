007d546cdd9e3e3f0202110f3e6bb959cd563c3c
jarilton <jariltonpereira02@hotmail.com>
2022-10-30 14:49:44 -0300

    Requisição pelo lado do servidor SSR(Server Side Rendering)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,8 @@ import tShirt from "../assets/img/tshirt.png";
 
 import "keen-slider/keen-slider.min.css";
 
-export default function Home() {
+export default function Home(props) {
+
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -18,6 +19,7 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
       <Product className="keen-slider__slide">
         <Image src={tShirt} width={520} height={480} alt="" />
 
@@ -55,4 +57,14 @@ export default function Home() {
       </Product>
     </HomeContainer>
   );
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 5000))
+
+  return {
+    props: {
+      list: [1, 2, 3, 4]
+    }
+  }
 }
