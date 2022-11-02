fe7c0d9b66cb39b8835e1d5d48dfab81d0a88bcd
jarilton <jariltonpereira02@hotmail.com>
2022-11-02 12:32:46 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from "next/router";
+import {
+  ImgContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Tela de produtos: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImgContainer>minha imagem...</ImgContainer>
+
+      <ProductDetails>
+        <h1>Detalhes do produto</h1>
+
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit earum
+          optio nam explicabo eligendi unde vitae rerum ipsa. Aliquid minima sed
+          praesentium illo similique, maiores sint blanditiis voluptas ex velit!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -10,7 +10,7 @@ export const HomeContainer = styled("main", {
 });
 
 export const Product = styled("a", {
-  background: "#ffffff",
+  background: "linear-gradient(180deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
   cursor: "pointer",
   position: "relative",
